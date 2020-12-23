b36647d64d6051c275938378be6880718b1cb233
vmikhina <viktoriia.mikhina@t-systems.com>
2020-12-23 15:52:27 +0300

    L-5. Added routes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
    
 
 @Injectable({
@@ -10,10 +11,22 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  
+  private heroesUrl = 'api/heroes';  // URL to web api
+
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero | undefined> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
