df13671ea4cab31209ba0903c14706c2060edc16
vmikhina <viktoriia.mikhina@t-systems.com>
2021-03-18 17:25:23 +0300

    Refactoring - bootstrap is used. Adding of hero works.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AddHeroComponent } from './add-hero/add-hero.component'
 
 
 const routes: Routes = [
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'add', component: AddHeroComponent },
 
 ];
 
